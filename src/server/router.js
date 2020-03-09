28af0192b10f22eb53a236236d1fc21af948f0cd
Marina <marinabulanzova@mail.ru>
2020-03-09 19:04:49 +0300

    server: books query

--- src/server/router.js
+++ src/server/router.js
@@ -1,6 +1,11 @@
 var express = require('express')
-var router = express.Router()
+var router = express()
 
-router.use('/books', require('./books/router'))
+router.listen(3000, function() {
+	console.log('API app started');
+})
 
+router.use('/books', require('./books/router.js'))
+//router.use('/clipboard', require('./clipboard/router.js'))
+//router.use('/base_element' require('./base_element/router.js'))
 module.exports = router
