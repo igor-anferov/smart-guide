c8a437683cc67d24f569a26668c8260596e17374
Igor Anferov <igor-anferov@mail.ru>
2020-04-23 18:07:11 +0300

    DB renamed

--- src/server/db/create.sql
+++ src/server/db/create.sql
@@ -1,210 +1,195 @@
 set DATESTYLE to "DMY";
 
 BEGIN;
-CREATE TABLE IF NOT EXISTS Elements (
-  id_element serial primary key,
+CREATE TABLE IF NOT EXISTS BaseElements (
+  element_id serial primary key,
   title varchar,
   category varchar,
   type varchar,
   key_element boolean,
   body bytea,
   source varchar,
-  id_group int,
-  id_author int,
-  date_added timestamp,
-  clipboard boolean 
+  group_id int,
+  author_id int,
+  created timestamp,
+  clipboard boolean
 );
 
-CREATE TABLE IF NOT EXISTS Objects (
-  id_object serial unique,
-  number_version int,
+CREATE TABLE IF NOT EXISTS Materials (
+  material_id serial primary key,
   snippet varchar,
-  id_group int,
-  id_author int,
-  date_added timestamp,
-  count_added int,
-  count_views int,
-  primary key(id_object, number_version)
+  group_id int,
+  author_id int,
+  created timestamp,
+  forks_count int,
+  views_count int
 );
 
-CREATE TABLE IF NOT EXISTS body_object (
-  id_object int,
-  number_version int,
-  id_element int 
+CREATE TABLE IF NOT EXISTS MaterialBaseElements (
+  material_id int,
+  position int,
+  element_id int
 );
 
-CREATE TABLE IF NOT EXISTS linked_object (
-  id_object1 int,
-  number_version1 int,
-  id_object2 int,
-  number_version2 int
+CREATE TABLE IF NOT EXISTS RelatedMaterials (
+  material_id int,
+  related_material_id int
 );
 
-CREATE TABLE IF NOT EXISTS  Questions (
-  id_question serial unique,
-  number_version int,
-  text_question varchar,
-  id_group int,
-  id_author int,
-  date_added timestamp,
-  primary key(id_question, number_version)
+CREATE TABLE IF NOT EXISTS Questions (
+  question_id serial primary key,
+  text varchar,
+  group_id int,
+  author_id int,
+  created timestamp
 );
 
-CREATE TABLE IF NOT EXISTS  answer (
-  id_question int,
-  number_version_q int,
-  id_object int,
-  number_version_o int
+CREATE TABLE IF NOT EXISTS QuestionMaterials (
+  question_id int,
+  position int,
+  material_id int
 );
 
-CREATE TABLE IF NOT EXISTS List_questions (
-  id_list_questions serial unique,
-  number_version int,
-  id_group int,
-  id_author int,
-  name_exam varchar,
-  name_teacher varchar,
-  date_added timestamp,
-  date_exam timestamp,
-  primary key(id_list_questions, number_version)
+CREATE TABLE IF NOT EXISTS Exams (
+  exam_id serial primary key,
+  group_id int,
+  author_id int,
+  title varchar,
+  teacher varchar,
+  created timestamp
 );
 
-CREATE TABLE IF NOT EXISTS array_answer (
-  id_list_questions int,
-  number_version1 int,
-  id_question int,
-  number_version2 int
+CREATE TABLE IF NOT EXISTS ExamQuestions (
+  exam_id int,
+  position int,
+  question_id int
 );
 
 CREATE TABLE IF NOT EXISTS Users (
-  id_user serial primary key,
+  user_id serial primary key,
   login varchar unique,
   hs256_sha256 char(64),
-  registration_ts timestamp,
   email varchar unique,
+  name varchar,
   university varchar,
-  faculty varchar
+  faculty varchar,
+  created timestamp
 );
 
-CREATE TABLE IF NOT EXISTS Books_PDF (
+CREATE TABLE IF NOT EXISTS Books (
   book_id serial primary key,
   title varchar,
-  id_user int,
+  user_id int,
   content bytea
 );
 
 CREATE TABLE IF NOT EXISTS Groups (
-  id_group serial primary key,
-  name_group varchar null,
-  id_creator int,
-  date_added timestamp
+  group_id serial primary key,
+  name varchar null,
+  creator_id int,
+  created timestamp
 );
 
-CREATE TABLE IF NOT EXISTS group_members (
-  id_user int,
-  id_group int,
+CREATE TABLE IF NOT EXISTS GroupMembers (
+  group_id int,
+  user_id int,
   date_entry timestamp,
   date_exit timestamp,
-  name_group varchar,
-  primary key(id_group, id_user)
+  primary key(group_id, user_id)
 );
 
-CREATE TABLE IF NOT EXISTS  Name_users (
-  id_user1 int,
-  id_user2 int,
-  name_user2 varchar,
-  primary key(id_user1, id_user2)
+CREATE TABLE IF NOT EXISTS Contacts (
+  user_id int,
+  contact_id int,
+  alias varchar,
+  primary key(user_id, contact_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS Comments (
-  id_comment serial primary key,
-  text_comment varchar,
-  id_object int,
-  id_group int,
-  id_author int,
-  date_added timestamp,
-  visibility_area int[]
+CREATE TABLE IF NOT EXISTS MaterialComments (
+  comment_id serial primary key,
+  text varchar,
+  material_id int,
+  group_id int,
+  author_id int,
+  created timestamp
 );
 
-CREATE TABLE IF NOT EXISTS tags_objects (
-  name_tag varchar,
-  id_object int,
-  id_group int,
-  id_author int,
-  date_added timestamp,
-  primary key(name_tag, id_object)
+CREATE TABLE IF NOT EXISTS MaterialTags (
+  material_id int,
+  tag varchar,
+  group_id int,
+  author_id int,
+  created timestamp,
+  primary key(material_id, tag)
 );
 
-CREATE TABLE IF NOT EXISTS tags_questions (
-  name_tag varchar,
-  id_question int,
-  id_group int,
-  id_author int,
-  date_added timestamp,
-  primary key(name_tag, id_question)
+CREATE TABLE IF NOT EXISTS QuestionTags (
+  question_id int,
+  tag varchar,
+  group_id int,
+  author_id int,
+  created timestamp,
+  primary key(question_id, tag)
 );
 
-CREATE TABLE IF NOT EXISTS tags_list_questions (
-  name_tag varchar,
-  id_list_questions int,
-  id_group int,
-  id_author int,
-  date_added timestamp,
-  primary key(name_tag, id_list_questions)
+CREATE TABLE IF NOT EXISTS ExamTags (
+  exam_id int,
+  tag varchar,
+  group_id int,
+  author_id int,
+  created timestamp,
+  primary key(exam_id, tag)
 );
 
-COMMIT;
 
-BEGIN; 
+alter table GroupMembers add
+	foreign key (user_id)
+	references Users(user_id),
+    add
+	foreign key (group_id)
+	references Groups(group_id);
 
-alter table group_members add
-	foreign key (id_user)
-	references Users(id_user),
-    add 
-	foreign key (id_group)
-	references Groups(id_group);
+alter table Books add
+	foreign key (user_id)
+	references Users(user_id);
 
-alter table Books_PDF add
-	foreign key (id_user)
-	references Users(id_user);
+alter table Contacts add
+	foreign key (user_id)
+	references Users(user_id),
+    add
+	foreign key (contact_id)
+	references Users(user_id);
 
-alter table name_users add
-	foreign key (id_user1)
-	references Users(id_user),
-    add 
-	foreign key (id_user2)
-	references Users(id_user); 
+/*alter table BaseElements add
+	foreign key (group_id, author_id)
+	references group_members(group_id, user_id);
 
-/*alter table Elements add
-	foreign key (id_group, id_author)
-	references group_members(id_group, id_user);
-
-alter table Objects add
-	foreign key (id_group, id_author)
-	references group_members(id_group, id_user);
+alter table Materials add
+	foreign key (group_id, author_id)
+	references group_members(group_id, user_id);
 
 alter table questions add
-	foreign key (id_group, id_author)
-	references group_members(id_group, id_user);
+	foreign key (group_id, author_id)
+	references group_members(group_id, user_id);
 
 alter table list_questions add
-	foreign key (id_group, id_author)
-	references group_members(id_group, id_user);
+	foreign key (group_id, author_id)
+	references group_members(group_id, user_id);
 
 alter table comments add
-	foreign key (id_group, id_author)
-	references group_members(id_group, id_user);
+	foreign key (group_id, author_id)
+	references group_members(group_id, user_id);
 
 alter table tags_objects add
-	foreign key (id_group, id_author)
-	references group_members(id_group, id_user);
+	foreign key (group_id, author_id)
+	references group_members(group_id, user_id);
 
 alter table tags_questions add
-	foreign key (id_group, id_author)
-	references group_members(id_group, id_user);
+	foreign key (group_id, author_id)
+	references group_members(group_id, user_id);
 
 alter table tags_list_questions add
-	foreign key (id_group, id_author)
-	references group_members(id_group, id_user);*/
+	foreign key (group_id, author_id)
+	references group_members(group_id, user_id);*/
 COMMIT;
