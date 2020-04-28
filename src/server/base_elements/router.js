8e381a5360ce0c0370449489599e644600d850da
Marina <marinabulanzova@mail.ru>
2020-04-28 17:14:20 +0300

    fix BaseElements

--- src/server/base_elements/router.js
+++ src/server/base_elements/router.js
@@ -9,13 +9,8 @@ router.get('/:base_element_id/info', async (req, res, next) => {
   try {
     const base_element_id = parseInt(req.params.base_element_id)
     const results = await pool.query(
-<<<<<<< HEAD
-      'SELECT title, source, type, is_pivotal FROM BaseElements WHERE element_id = $1;',
-      [element_id]
-=======
-      'SELECT title, source, type FROM BaseElements WHERE base_element_id = $1;',
+      'SELECT title, source, type, is_pivotal FROM BaseElements WHERE base_element_id = $1;',
       [base_element_id]
->>>>>>> eeedaeca1050fc8b3a4efa09e70149ac598fd953
     )
     res.status(200).send(results.rows[0])
   } catch (e) {
@@ -27,13 +22,8 @@ router.get('/:base_element_id/content', async (req, res, next) => {
   try {
     const base_element_id = parseInt(req.params.base_element_id)
     const results = await pool.query(
-<<<<<<< HEAD
-      'SELECT body, type FROM BaseElements WHERE element_id = $1;',
-      [element_id]
-=======
-      'SELECT body, type FROM Elements WHERE base_element_id = $1;',
+      'SELECT body, type FROM BaseElements WHERE base_element_id = $1;',
       [base_element_id]
->>>>>>> eeedaeca1050fc8b3a4efa09e70149ac598fd953
     )
     const result = results.rows[0]
     switch (result.type) {
@@ -61,13 +51,8 @@ router.post('/:base_element_id', async (req, res, next) => {
         Object.keys(args)
           .map((k, i) => k + ` = $${i + 1}`)
           .join()
-<<<<<<< HEAD
       } WHERE element_id = $${Object.entries(args).length + 1}`,
       [...Object.values(args), element_id]
-=======
-      } WHERE id_element = $${Object.entries(args).length + 1}`,
-      [...Object.values(args), base_element_id]
->>>>>>> eeedaeca1050fc8b3a4efa09e70149ac598fd953
     )
     res.status(200).send()
   } catch (e) {
